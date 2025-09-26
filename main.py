e3b9c1612076fb09ebac4b9037d15a22ab12a850
aisavage-stack <aisavage@aggies.ncat.edu>
2025-09-26 18:05:49 -0400

    git add aisavage_assignment_3.py
    git commit -m "Pass Test Case 1: Initial game setup with required variables"

---
+++ main.py
@@ -0,0 +1,14 @@
+student_name = "Aiyana"  
+current_gpa = 4.0
+study_hours = 5
+social_points = 40 
+stress_level = 65
+
+print("Welcome! Here are your stats:")
+print(f"Current GPA: {current_gpa}")
+print(f"Study Hours: {study_hours}")
+print(f"Social Points: {social_points}")
+print(f"Stress Level: {stress_level}")
+
+git add aisavage_assignment_3.py
+git commit -m "Pass Test Case 1: Initial game setup with required variables"
